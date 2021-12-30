754da0510703795718b1af7e408125452bddd757
codnjs779 <codnjs779@naver.com>
2021-12-31 00:55:23 +0900

    app component로 state이동 & 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import "./App.css";
-import Habit from "./components/habit";
 import Habits from "./components/habits";
 
-function App() {
-    return <Habits />;
+import React, { Component } from "react";
+
+class App extends Component {
+    state = {
+        habits: [
+            { id: 0, name: "Running", count: 0 },
+            { id: 1, name: "Reading", count: 0 },
+            { id: 2, name: "Coding", count: 0 },
+        ],
+    };
+    onCounter = () => {
+        console.log("hello");
+    };
+    render() {
+        return (
+            <>
+                <header>
+                    ✅ Habits Tracker
+                    <span>0</span>
+                </header>
+
+                <main>
+                    <form>
+                        <input type="text" placeholder="Habit" />
+                        <button type="submit">Add</button>
+                    </form>
+                    <Habits state={this.state} />
+                </main>
+
+                <button className="resetBtn">Reset All</button>
+            </>
+        );
+    }
 }
 
 export default App;
